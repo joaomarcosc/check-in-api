e384e8d23f8f628459bef0a502e1753de139b775
joaomarcos <j.marcos9@hotmail.com>
2024-03-12 20:26:11 -0300

    feat: create check-in feature

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,45 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-in'
+import { randomUUID } from 'crypto'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: CheckInUseCase
+
+describe('Authentication use case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('Should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: randomUUID(),
+      userId: randomUUID(),
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('Should not be able to check in twice in same day', async () => {
+    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
