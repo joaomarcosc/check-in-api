a12d5e19d5f97c04c22849542ae65b62706bfbc4
joaomarcos <j.marcos9@hotmail.com>
2024-03-24 20:11:41 -0300

    test: add new test on check in feature

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -42,4 +42,22 @@ describe('Authentication use case', () => {
       }),
     ).rejects.toBeInstanceOf(Error)
   })
+
+  it('Should not be able to check in twice but in difference days', async () => {
+    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    vi.setSystemTime(new Date(2023, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
 })
