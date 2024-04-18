5961e622f14b6764169d20b57d73d73e2fed59bb
joao.carvalho <joao.carvalho@cubos.io>
2024-04-18 13:58:24 -0300

    refactor: adjust coordinate function

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,6 +1,12 @@
 import { Gym, Prisma } from '@prisma/client'
 
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
+
 export interface GymsRepository {
   findById(gymId: string): Promise<Gym | null>
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
   create(data: Prisma.GymCreateInput): Promise<Gym>
 }
